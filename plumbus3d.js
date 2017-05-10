91c3fed8401a5d264bba4e080ba4b45db134e6d7
Jack Boffa <jaffob77@gmail.com>
2017-05-09 22:10:17 -0700

    2D drawing done, starting keyboard input

--- plumbus3d.js
+++ plumbus3d.js
@@ -1,18 +1,57 @@
-var x = 400.0, y = 300.0;	// X,Y coordinates of the player.
-var dir = 0.0;				// Direction player is facing, in radians.
+// Gameplay constants.
+var FPS = 10;
+var PI = Math.PI;
 
+// 2D drawing constants.
+var C2D_PLAYER_RADIUS = 10;
+
+// Define the player and its values.
+var player = {
+
+	x: 400, y: 300,		// Current coordinates
+	dir: 0,				// Current direction in radians
+	vf: 200,			// Movement speed, per second
+	vr: PI				// Rotation speed, radians per second
+}
+
+// Define the array of walls.
 var walls = [
-{x1:200,y1:200,x2:300,y2:200},
+{x1:200,y1:200,x2:400,y2:200},
 {x1:500,y1:200,x2:500,y2:400}
 ];
 
+function start()
+{
+	setInterval(run, 1000/FPS);
+}
+
+function run()
+{
+	draw();
+}
+
 function draw()
 {
-	var c2d = document.getElementById("canvas2d").getContext("2d");
+	draw2d("canvas2d");
+}
+
+function draw2d(canvas_id)
+{
+	var c2d = document.getElementById(canvas_id).getContext("2d");
 	
-	console.log("drawing now");
-	c2d.moveTo(100,100);
-	c2d.lineTo(200,200);
+	// Draw player in 2D.
+	c2d.beginPath();
+	c2d.arc(player.x, player.y, C2D_PLAYER_RADIUS, 0, 2 * Math.PI, false);
+	c2d.fillStyle = "#FF0000";
+	c2d.strokeStyle = "#FF0000";
+	c2d.closePath();
+	c2d.fill();
+	c2d.stroke();
+	
+	// Draw player direction in 2D.
+	c2d.strokeStyle = "#000000";
+	c2d.moveTo(player.x, player.y);
+	c2d.lineTo(player.x + C2D_PLAYER_RADIUS * 2 * Math.cos(player.dir), player.y + C2D_PLAYER_RADIUS * 2 * Math.sin(player.dir));
 	c2d.stroke();
 	
 	for (var i = 0; i < walls.length; i++)
@@ -22,3 +61,8 @@ function draw()
 		c2d.stroke();
 	}
 }
+
+window.addEventListener('keydown', function(event)
+{
+
+});
